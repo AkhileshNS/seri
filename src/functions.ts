a9c6408b7384dcaf6152eaf358b8e05a31400962
AkhileshNS <nsakhilesh02@gmail.com>
2021-11-22 15:34:29 -0500

    Refined the output of the ast parser

--- src/functions.ts
+++ src/functions.ts
@@ -16,7 +16,34 @@ export const getOptions = (): IOptions => {
   return {path: path.join(process.cwd(), passedPath)}
 }
 
+// [IN-PLACE] USE WITH CARE
+// Recursive function to remove "start" and "end"
+const removeStartAndEnd = (obj: any) => {
+  for (let prop in obj) {
+    if (prop==="start" || prop==="end") {
+      delete obj[prop];
+    }
+    if (Object.prototype.toString.call(obj[prop]) === "[object Object]") {
+      removeStartAndEnd(obj[prop]);
+    }
+    if (Object.prototype.toString.call(obj[prop]) === "[object Array]") {
+      for (let value of obj[prop]) {
+        removeStartAndEnd(value);
+      }
+    }
+  }
+}
+
+// The custom stringify just does JSON.stringify but also removes any "start" and "end" properties
+const customStringify = (ast: acorn.Node) => {
+  const parsedAst = JSON.parse(JSON.stringify(ast));
+  removeStartAndEnd(parsedAst);
+  return JSON.stringify(parsedAst, null, 2);
+}
+
 // Function to get the stringified AST of some js code
 // ("const a = 1;") -> <AST> | To see an example of an AST output for `acorn`, check out https://astexplorer.net/
 export const getAST = (content: string): string => 
-  JSON.stringify(parse(content, {ecmaVersion: 2020, locations: false, ranges: false}), null, 2); 
+  customStringify(parse(content, {
+    ecmaVersion: 2020, locations: false, ranges: false
+  })); 
